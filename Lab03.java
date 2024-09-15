652613f569055c2e1886fcd132f38b741463cf10
John Doe <johndoe@email.com>
2024-09-15 17:09:29 +0300

    Main and studnets have been added

---
+++ Lab03.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        String[] student = new String[5];
+        student[0]= "Ahmed";
+        student[1]= "Ali";
+        student[2]="saleh";
+        student[3]="Yasser";
+        student[4]="Nolan";
+        readStudents(student);
+        randomgroups(student);
+
+
+    }
+
+}
